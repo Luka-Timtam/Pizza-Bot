f883d0aef5b77a9aefef14d81fe0239e7742af56
Luka-Timtam <132308048+Luka-Timtam@users.noreply.github.com>
2023-05-16 09:40:42 +1200

    Added comments for each section
    Comments for each section

--- pizza_bot.py
+++ pizza_bot.py
@@ -1,2 +1,69 @@
-#Pizza Bot Program
+# Pizza Bot Program
 
+# Welcome message with random name
+
+
+
+
+
+
+# Menu for pickup or delivery
+
+
+
+
+
+
+# Pick up information
+
+
+
+
+
+
+# Delivery information - name address and phone
+
+
+
+
+
+# Choose total number of pizzas - max 5
+
+
+
+
+
+
+# Pizza menu 
+
+
+
+
+
+
+# Pizza order - from menu - print each pizza ordered with cost
+
+
+
+
+
+# Print order out - including if order is delivery or pickup and names and price of each pizza  - total cost including any delivery charge
+
+
+
+
+# Ability to cancel or proceed with order
+
+
+
+
+
+# Option for new order or to exit
+
+
+
+
+
+
+
+# Main function
