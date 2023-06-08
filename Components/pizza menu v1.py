b4be0f8dca2ee2ac226fcd27781b292112fa9792
Luka-Timtam <132308048+Luka-Timtam@users.noreply.github.com>
2023-06-08 15:12:13 +1200

    8/06/23
    Alot of 15 and some of 16

---
+++ Components/pizza menu v1.py
@@ -0,0 +1,4 @@
+pizza_names = ['Margherita','Pepperoni','Hawaiian','Cheese','Italian','Veggie','Vegan','Chicken Deluxe',
+               'Mega Meat Lovers','Seafood Deluxe','Apricot Chicken Deluxe','BBQ Chicken Deluxe']
+
+#lesson 16 15.49
--- pizza_bot.py
+++ pizza_bot.py
@@ -50,12 +50,11 @@ def order_type():
             if delivery >= 1 and delivery <= 2:
                 if delivery == 1:
                     print ("Pickup")
-                    pickup()
+                    pickup_info()
                     break
-                    
-
                 elif delivery == 2:
                     print ("Delivery")
+                    delivery_info()
                     break
             else:
                 print ("The number must be 1 or 2")
@@ -67,23 +66,42 @@ def order_type():
 
 
 # Pick up information
+def pickup_info():
+    question = ("Please enter your name: ")
+    customer_details['name'] = not_blank(question)
+    print (customer_details['name'])
 
+    question = ("Please enter your phone number: ")
+    customer_details['phone'] = not_blank(question)
+    print (customer_details['phone'])
+    print(customer_details)
 
 
 
 
 
 # Delivery information - name address and phone
-def pickup():
+def delivery_info():
     question = ("Please enter your name: ")
     customer_details['name'] = not_blank(question)
-    #print (customer_details['name'])
+    print (customer_details['name'])
 
     question = ("Please enter your phone number: ")
     customer_details['phone'] = not_blank(question)
-    #print (customer_details['phone'])
-    print(customer_details)
+    print (customer_details['phone'])
 
+    question = ("Please enter your house number: ")
+    customer_details['house'] = not_blank(question)
+    print (customer_details['house'])
+
+    question = ("Please enter your street: ")
+    customer_details['street'] = not_blank(question)
+    print (customer_details['street'])
+
+    question = ("Please enter your suburb: ")
+    customer_details['suburb'] = not_blank(question)
+    print (customer_details['suburb'])
+    print(customer_details)
 
 
 # Choose total number of pizzas - max 5
